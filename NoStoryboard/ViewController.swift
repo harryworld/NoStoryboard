3d5d5a92c80c3b972e6d79eaec162c2874cd43d1
Harry Ng <harryworld@gmail.com>
2018-08-02 01:03:01 +0800

    Initial Commit

---
+++ NoStoryboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoStoryboard
+//
+//  Created by Harry Ng on 2/8/2018.
+//  Copyright © 2018 StaySorted Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
